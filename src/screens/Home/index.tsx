b2d10ed9277ee08c16978a3f8973dd8167e77fb8
matheus-andrade <mthrafaelandrade@gmail.com>
2025-04-14 13:04:25 -0300

    criado componente de participante

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,17 +1,47 @@
-import { Text, View } from "react-native";
+import { Text, View, TextInput, TouchableOpacity } from "react-native";
 
 import { styles } from "./style";
+import { Participant } from "../components/participant";
 
 export default function Home() {
+  function handleParticipantAdd() {
+    console.log("aaaaaaaa");
+  }
+
+  function handleParticipantAddRemove(name: string) {
+    console.log("aaaaaaaa");
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.eventName}>
-        Nome do evento
-      </Text>
+      <Text style={styles.eventName}>Nome do evento</Text>
 
-      <Text style={styles.eventDate}>
-        Sexta, 4 de Novembro de 2022.
-      </Text>
+      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022.</Text>
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor={"#6B6B6B"}
+        ></TextInput>
+
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Participant
+        name="Matheus Andrade"
+        onRemove={() => handleParticipantAddRemove("Matheus Andrade")}
+      ></Participant>
+      <Participant
+        name="Carlos Alberto"
+        onRemove={() => handleParticipantAddRemove("Carlos Alberto")}
+      ></Participant>
+      <Participant
+        name="Cheguevaras Ronaldo"
+        onRemove={() => handleParticipantAddRemove("Cheguevaras Ronaldo")}
+      ></Participant>
     </View>
-  )
+  );
 }
--- src/screens/Home/style.ts
+++ src/screens/Home/style.ts
@@ -3,17 +3,45 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#131016',
-    padding: 24
+    backgroundColor: "#131016",
+    padding: 24,
   },
   eventName: {
-    color: '#FDFCFE',
+    color: "#FFF",
     fontSize: 24,
-    fontWeight: 'bold',
-    marginTop: 48
+    fontWeight: "bold",
+    marginTop: 48,
   },
   eventDate: {
-    color: '#6B6B6B',
-    fontSize: 16
-  }
-})
+    color: "#6B6B6B",
+    fontSize: 16,
+  },
+  input: {
+    height: 52,
+    backgroundColor: "#1F1E25",
+    borderRadius: 5,
+    color: "#FFF",
+    padding: 16,
+    fontSize: 16,
+    flex: 1,
+    marginRight: 12,
+  },
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: "#31CF67",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    color: "#FFF",
+    fontSize: 24,
+  },
+  form: {
+    width: "100%",
+    flexDirection: "row",
+    marginTop: 36,
+    marginBottom: 42,
+  },
+});
